4d5f06f2cb4b60e8cefc7e2132bce9472b76e73d
pedro lozano <perico1597@hotmail.com>
2022-08-25 00:54:20 +0200

    ejercicio 2

---
+++ Ejercicio-2/ejercicio-2.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Perico Lozano",
+    35,
+    false,
+    new Date(1987,4,23),
+    {
+        titulo: "El codigo da vinci",
+        autor: "Brown,Dan",
+        fecha: new Date(2005, 4, 15),
+        url:"https://www.iberlibro.com/Codigo-Vinci-Brown-Dan-Estrella-Juanjo/"
+    },
+];
