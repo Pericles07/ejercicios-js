929a97baa2e5047e9c8e4cff4021df9b6619c0cd
pedro lozano <perico1597@hotmail.com>
2022-08-30 22:50:41 +0200

    ejercicio-04

---
+++ Ejercicio-4/ejercicio-04.js
@@ -0,0 +1,12 @@
+
+const nombre = "Pedro";
+const apellido = "Lozano";
+const estudiante = nombre + " " + apellido;
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const estudianteLength = estudiante.length
+const letraInicial = nombre[0];
+const letraFinal = apellido[apellido.length -1];
+const sinEspacios = estudiante.replace(/ /g,"");
+const nombreIncluido = estudiante.includes(nombre);
+
