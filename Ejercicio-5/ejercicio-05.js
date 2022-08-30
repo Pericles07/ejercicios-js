f6a66b2e904b5c632af0c723de44ec187dc52d8e
pedro lozano <perico1597@hotmail.com>
2022-08-31 01:06:03 +0200

    ejercicio-5

---
+++ Ejercicio-5/ejercicio-05.js
@@ -0,0 +1,6 @@
+const altura_cm = 172;
+const altura_metro = 1.72;
+const peso_kg = 68.5;
+const altura_red = Math.ceil(altura_metro);
+const peso_red = Math.floor(peso_kg);
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
